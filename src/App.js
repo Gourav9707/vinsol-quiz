b989c8e4ca0d727705abbddb21be85ecde0a4862
Gsaha9707 <sahagourav9@gmail.com>
2021-09-12 14:10:03 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,129 @@
+import React from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import PageShell from "./components/PageShell";
+import { QUESTION_LIMIT, OPERATORS } from "./utils/miscUtils";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    width: "100%"
+  }
+}))
+
+const App = () => {
+  const classes = useStyles();
+
+  const [allQuestions, setAllQuestions] = React.useState({quiz1: [], quiz2: []});
+  const [activeQuestion, setActiveQuestion] = React.useState({quiz1: {}, quiz2: {}});
+  const [score, setScore] = React.useState({quiz1: 0, quiz2: 0});
+  const [endQuiz, setEndQuiz] = React.useState({quiz1: false, quiz2: false});
+
+  const _shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+                    
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+        
+    return array;
+ }
+
+  const _generateWrongOptions = (ans) => {
+    let num = Math.floor(Math.random() * 90);
+    return (num === ans)
+      ? _generateWrongOptions(ans)
+      : {
+        value: num,
+        correct: false,
+        clicked: false
+    };
+}
+
+  const generateQuestion = (name) => {
+    if(endQuiz[name]) return
+    const selectedOperator = OPERATORS[Math.floor(Math.random() * OPERATORS.length)];
+    const operand1 = Math.floor(Math.random() * 10);
+    const operand2 = Math.floor(Math.random() * 10);
+    
+    if (isFinite(eval(`${operand1 + selectedOperator + operand2}`))) {
+      let ans = [{
+        value: eval(`${operand1 + selectedOperator + operand2}`),
+        correct: true,
+        clicked: false
+      }]
+      while (ans.length < 4) 
+        ans.push(_generateWrongOptions(eval(`${operand1 + selectedOperator + operand2}`)));
+
+      setActiveQuestion({
+        ...activeQuestion,
+        [name]: {
+          question: `${operand1 + selectedOperator + operand2}`,
+          options: _shuffleArray(ans)
+        }
+      })
+    } else generateQuestion(name);
+  }
+
+  const handleOptionClick = (question, options, option, name) => {
+    option.correct && setScore({ ...score, [name]: score[name] ? score[name] + 1 : 1 });
+    const filteredOptions = [];
+    for (const data of options) {
+      if (data.value === option.value)
+        filteredOptions.push(option)
+      else
+        filteredOptions.push(data)
+    }
+    setAllQuestions({
+      ...allQuestions,
+      [name]: [...allQuestions[name], {
+        question,
+        options: filteredOptions
+      }]
+    });
+    if (allQuestions[name].length === QUESTION_LIMIT) {
+      setEndQuiz({ ...endQuiz, [name]: true });
+      return;
+    }
+    generateQuestion(name)
+  }
+
+  const handleNextClick = (name, question, options) => {
+    setAllQuestions({
+      ...allQuestions,
+      [name]: [...allQuestions[name], {
+        question,
+        options
+      }]
+    });
+    if (allQuestions[name].length === QUESTION_LIMIT) {
+      setEndQuiz({ ...endQuiz, [name]: true });
+      return;
+    }
+    generateQuestion(name)
+  }
+
+  return (
+    <Box className={classes.root}>
+      {Object.keys(activeQuestion).length &&
+        Object.keys(activeQuestion).map((screen, index) => (
+          <PageShell
+            title={screen.toString().toUpperCase()}
+            question={activeQuestion[screen].question || ""}
+            options={activeQuestion[screen].options || []}
+            handleOptionClick={handleOptionClick}
+            generateQuestion={generateQuestion}
+            handleNextClick={handleNextClick}
+            endQuiz={endQuiz[screen]}
+            allQuestions={allQuestions[screen]}
+            score={`${score[screen]}/${allQuestions[screen].length}`}
+          />
+      ))}
+    </Box>
+  )
+}
+
+export default App;
+
---
+++ src/utils/miscUtils.js
@@ -0,0 +1,24 @@
+const COLOR_CODE = [
+  {
+    code: "#ff9999",
+    text: "Wrong Answer Marked"
+  },
+  {
+    code: "#ccffcc",
+    text: "Correct Answer marked"
+  },
+  {
+    code: "#00ff55",
+    text: "Right Answer"
+  },
+];
+
+const OPERATORS = ["*", "-", "/", "+"];
+
+const QUESTION_LIMIT = 19;
+
+export {
+  COLOR_CODE,
+  OPERATORS,
+  QUESTION_LIMIT
+}
